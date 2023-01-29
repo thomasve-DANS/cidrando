c79c146babc4031f39d92dafc55b6cbc5890a3b4
Thomas van Erven <thomasvanerven@danss-mbp-2.kpn>
2023-01-29 17:31:35 +0100

    Fixing middleware.

--- src/main.py
+++ src/main.py
@@ -2,22 +2,37 @@ from typing import List, Optional
 from fastapi import FastAPI, Response
 import random
 import copy
+from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
 
 class Data(BaseModel):
     leaders: List[str]
-    players: List[str] 
+    players: List[str]
     amount_per_player: int = 1
 
 class Result(BaseModel):
     leaders: List[str]
-    players: List[str] 
+    players: List[str]
     amount_per_player: int = 1
     choices: dict
     message: Optional[str]
 
+origins = [
+    'http://127.0.0.1:5173',
+    'https://127.0.0.1:5173'
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    origins=origins,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/randomize")
 def randomize(input_data: Data, response: Response):
@@ -40,5 +55,3 @@ def randomize(input_data: Data, response: Response):
                 )
         results[player] = sorted(choices)
     return results
-
-    
